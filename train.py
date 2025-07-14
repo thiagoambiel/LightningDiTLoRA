1dc8b264ab50e7faee891a5ac25f589ea17d58e3
Thiago Ambiel <thiago.ambiel1@gmail.com>
2025-07-14 15:09:19 -0300

    Optimize only Attention & Embeddings

--- train.py
+++ train.py
@@ -101,12 +101,16 @@ def do_train(train_config, accelerator):
         use_checkpoint=train_config['model']['use_checkpoint'] if 'use_checkpoint' in train_config['model'] else False,
     )
 
-    # for param in model.parameters(): 
-    #     param.requires_grad = False
+    for param in model.parameters(): 
+        param.requires_grad = False
 
-    # for name, param in model.blocks.named_parameters():
-    #     if any([layer in name for layer in ['qkv', 'proj']]):
-    #         param.requires_grad = True
+    for name, param in model.blocks.named_parameters():
+        if any([layer in name for layer in ['qkv', 'proj']]):
+            param.requires_grad = True
+        
+    for module in [model.x_embedder, model.y_embedder, model.t_embedder]:
+        for param in module.parameters(): 
+            param.requires_grad = True
 
     ema = deepcopy(model).to(device)  # Create EMA model
 
